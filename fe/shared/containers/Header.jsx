649e46ecda43b0504c8aa9e6685127592db94b4c
Chihung Yu <cyu0@walmartlabs.com>
2016-02-22 21:41:06 -0800

    added navigation for the header

--- fe/shared/containers/Header.jsx
+++ fe/shared/containers/Header.jsx
@@ -6,8 +6,6 @@ import {
   requestLogoutAct
 } from '../actions/Session'
 
-const STYLES = {};
-
 class Header extends React.Component {
   constructor(props) {
     super(props);
@@ -29,26 +27,27 @@ class Header extends React.Component {
     return (
       <nav id='header' className='bg-primary clearfix'>
         <div className='nav-brand'>
-          <Link to='/' style={STYLES.navBrandLink}>Modulator</Link>
+          <Link to='/'>Modulator</Link>
         </div>
-        <div className='nav-profile primary last clearfix' onClick=''>
+        <div className='nav-profile profile last clearfix'>
           <div className='pull-left'><i className="fa fa-user icon"></i></div>
           <div className='pull-left'>Welcome, <br />{ name }</div>
           <ul className='nav-profile-content'>
-            <li>
-              <Link to='/settings'>Settings</Link>
-            </li>
-            <li>
-              <Link to='/profile'>Profile</Link>
-            </li>
+            <li><Link to='/settings'>Settings</Link></li>
+            <li><Link to='/profile'>Profile</Link></li>
             <hr />
-            <li onClick={this.handleLogout}>
-              <a>Logout</a>
-            </li>
+            <li onClick={this.handleLogout}><a>Logout</a></li>
           </ul>
         </div>
-        <div className='nav-profile secondary'>
-          
+        <div className='nav-profile info clearfix'>
+          <div className='pull-left'><i className="fa fa-bell"></i></div>
+        </div>
+        <div className='nav-profile menu clearfix'>
+          <div className='pull-left'><i className="fa fa-bars"></i></div>
+          <ul className='nav-profile-content'>
+            <li><Link to='/dashboard'>Dashboard</Link></li>
+            <li><Link to='/team'>Team</Link></li>
+          </ul>
         </div>
       </nav>
     );
